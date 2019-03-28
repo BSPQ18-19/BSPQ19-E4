848f32173e5bcfb31501e7db8e3a691eb38f7151
aneinfante <ane.infante@opendeusto.es>
2019-03-28 23:34:17 +0100

    fixed maven structure, but scr still not recognised

---
+++ CarRentingServer/src/main/java/com/bspq/team4/App.java
@@ -0,0 +1,13 @@
+package com.bspq.team4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
